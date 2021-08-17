3672071021860e7191fe5669e03fc2cab4be5813
eric <swimleft@gmail.com>
2021-08-17 00:43:22 -0700

    second commit

--- Server/ProxyServer/server.js
+++ Server/ProxyServer/server.js
@@ -11,19 +11,31 @@ const app = express()
 
 app.use(cors());
 
+//app.use(express.static('public'))
+app.get("/proxy/words", async (req,res) => {
+    const url= "http://loripsum.net/api/10/short/headers"
+    console.log(url)
+ 
+     try{
+         const data = await axios.get(url)
+         res.json(data.data)
+     } catch(err){
+         res.send("could not complete")
+       //  console.log(err)
+     }
+ })
+
 app.get("/proxy", async (req,res) => {
    const url= req.query.url
    console.log(url)
 
     try{
-        
         const data = await axios.get(url)
-
         res.json(data.data)
     } catch(err){
+        res.send("could not complete")
       //  console.log(err)
     }
-    
 })
 
 app.get("/",(req,res) => {
